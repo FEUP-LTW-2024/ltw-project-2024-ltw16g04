b4871808be67b801d68bd2e7e59a0da75bdd50f8
Leonardo Garcia <leonardorsg@hotmail.com>
2024-04-13 15:54:08 +0100

    item page done

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const favoriteButton = document.querySelector(".favorite-button");
-favoriteButton.addEventListener("click", (event) => {
-  const button = event.currentTarget;
-  button.classList.toggle("is-favorite");
-});
