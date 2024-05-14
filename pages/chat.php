1fa07f17ad9965ccb778be2703544d22315ba48d
Marcel Medeiros <marcel_segundo@outlook.com>
2024-05-14 15:56:24 +0100

    Chat fixed, security missing

--- pages/chat.php
+++ pages/chat.php
@@ -100,6 +100,7 @@
         <div class="chat-container">
           <div id="chat-box"></div>
           <form id="message-form">
+      
             <input type="text" id="message-input" placeholder="Type your message...">
             <button type="submit">Send</button>
           </form>
